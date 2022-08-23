d4b8c9aaa7b980722b66ab224a38855c2a5d3b9d
Laziz Djuraev <110926594+laziz511@users.noreply.github.com>
2022-08-23 16:01:23 +0500

    Update FibonacciSeries.java

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        if (lastFibonacci==1){
+            System.out.println("0");
+            return;
+        }else{
+            int first = 0, second = 1, third;
+            for(int i=1; i<=lastFibonacci; i++){
+                System.out.println(first);
+                third = second + first;
+                first = second;
+                second = third;
+            }
+        }
+        
+        
     }
 }
