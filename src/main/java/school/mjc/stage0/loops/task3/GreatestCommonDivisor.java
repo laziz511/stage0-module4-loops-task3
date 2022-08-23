9e71944a01cf9dd40fb50c60a8258710192c8067
Laziz Djuraev <110926594+laziz511@users.noreply.github.com>
2022-08-23 16:00:05 +0500

    Update GreatestCommonDivisor.java

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+        int max = Math.max(first,second);
+        int min  = Math.min(first,second);
+        for(int i = min; i>0; i--){
+            if(max%i ==0 && min%i==0){
+                System.out.println(i);
+                return;
+            }
+        }
     }
 }
