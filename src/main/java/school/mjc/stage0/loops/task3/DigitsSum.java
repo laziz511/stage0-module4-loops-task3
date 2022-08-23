3423937df06fd14c39b9f15ce333f65ae2deab24
Laziz Djuraev <110926594+laziz511@users.noreply.github.com>
2022-08-23 16:05:47 +0500

    Update DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -3,9 +3,9 @@ package school.mjc.stage0.loops.task3;
 public class DigitsSum {
     public void printDigitsSum(int t){
         int summa = 0;
-        while(number>0){
-            summa += number%10;
-            number /=10;
+        while(t>0){
+            summa += t%10;
+            t /=10;
         }
         System.out.println(summa);
 
