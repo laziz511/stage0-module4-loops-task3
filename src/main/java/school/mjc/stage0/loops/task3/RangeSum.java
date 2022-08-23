af936716431c647200ee43d8a34b3256d707a1ca
Laziz Djuraev <110926594+laziz511@users.noreply.github.com>
2022-08-23 15:58:32 +0500

    Update RangeSum.java

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
+        int summa = 0;
+        for(; firstBoarder<=secondBoarder; firstBoarder++){
+            summa += firstBoarder;
+        }
+        System.out.println(summa);
     }
 }
