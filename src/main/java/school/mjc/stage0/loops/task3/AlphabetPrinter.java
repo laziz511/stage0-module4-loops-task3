10f59ea94c13b75641b26d6497652d34f5a72933
Laziz Djuraev <110926594+laziz511@users.noreply.github.com>
2022-08-23 15:59:10 +0500

    Update AlphabetPrinter.java

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+        for (int i = 'a'; i <= 'z'; i++)
+            System.out.println((char) i);
     }
 }
